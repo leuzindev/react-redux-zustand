7732eccb189dbe6fc848a5c30564dac0cf69b760
Leonardo_Soares <leonardoc.soares08@gmail.com>
2023-07-07 17:16:44 -0300

    feat: add test with vitest

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { player } from './slices/player'
+import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+
+export const store = configureStore({
+  reducer: {
+    player,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
